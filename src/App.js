8c6b87da9b0378408e1405b2382a85aefba7d739
Jackie A <prophetessjnc@gmail.com>
2021-08-26 17:15:35 -0400

    CHNG: additional title added header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import './App.css';
 import Header from './Header';
 
 function App() {
-  const title = 'Amazon Cart';
   
   return (
     <div className="App">
-      <Header title={title}/>
+      <Header title="Amazon Cart"/>
     </div>
   );
 }
