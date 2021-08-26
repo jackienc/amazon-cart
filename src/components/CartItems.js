80de5b6247e0eab960da688850c4feb97c82fb39
Jackie A <prophetessjnc@gmail.com>
2021-08-26 19:12:08 -0400

    ADD: CartItem component structure

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import CartItem from './CartItem'
 import './CartItems.css'
 
 function CartItems() {
     return (
         <div className="CartItems">
         <h1>Shopping Cart</h1>
+        <hr />
+        <div className="CartItems-items">
+            <CartItem />
+        </div>
         </div>
     )
 }
