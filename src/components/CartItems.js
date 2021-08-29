34211ef0fe44ff1980687ff6cc1f5ea49e840710
Jackie A <prophetessjnc@gmail.com>
2021-08-28 20:50:20 -0400

    ADD: Loop through CartItems & images in from public folder

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -3,13 +3,18 @@ import CartItem from './CartItem'
 import './CartItems.css'
 
 function CartItems({items}) {
-    console.log('inside cart items component', items)
+console.log(items)
     return (
         <div className="CartItems">
         <h1>Shopping Cart</h1>
         <hr />
         <div className="CartItems-items">
-            <CartItem />
+            {items.map((item, index) => 
+                <CartItem 
+                key={index}
+                item={item}
+                />     
+            )}
         </div>
         </div>
     )
