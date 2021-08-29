dace0fed4e3a29cb8b80a474efe82f59ed6bdbd7
Jackie A <prophetessjnc@gmail.com>
2021-08-28 22:55:36 -0400

    ADD: CartTotal HTML/Style

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import items from '../Data'
 import './CartTotal.css'
 
 function CartTotal() {
     return (
         <div className="CartTotal">
-        
+            <h3>Subtotal (5 items):
+                <span className="CartTotal-price">
+                        $2,172.38
+                </span>
+            </h3>
+            <button>
+                Proceed to checkout
+            </button>
         </div>
     )
 }
